5d633920c9f011c3fd2fa2020814be63a7ae9ddc
BasedOnCpp <ivan.boriso2014@gmail.com>
2023-04-03 21:48:58 +0300

    optimized rectangle drawing

---
+++ BmpLib/bmp_24.h
@@ -0,0 +1,42 @@
+#include <stdint.h>
+
+;
+#pragma pack(push, 1)
+
+typedef struct tagBITMAPFILEHEADER {
+	uint16_t bfType;
+	uint32_t bfSize;
+	uint16_t bfReserved1;
+	uint16_t bfReserved2;
+	uint32_t bfOffBits;
+} BITMAPFILEHEADER;
+
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+
+typedef struct tagBITMAPINFOHEADER {
+	int32_t biSize;
+	int32_t biWidth;
+	int32_t biHeight;
+	uint16_t biPlanes;
+	uint16_t biBitCount;
+	uint32_t biCompression;
+	uint32_t biSizeImage;
+	uint32_t biXPelsPerMeter;
+	uint32_t biYPelsPerMeter;
+	uint32_t biClrUsed;
+	uint32_t biClrImportant;
+} BITMAPINFOHEADER;
+
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+
+typedef struct tagRGB {
+	uint8_t b;
+	uint8_t g;
+	uint8_t r;
+} RGB;
+
+#pragma pack(pop)
